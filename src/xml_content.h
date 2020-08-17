486d852419b4e733d16ab6ffbcc43678cf64bf74
kiishor <kiishor@icloud.com>
2020-08-17 20:42:34 +0530

    Handling of choice order is corrected

--- src/xml_content.h
+++ src/xml_content.h
@@ -52,7 +52,9 @@ ADD_DATA_TYPE(XS_DURATION,             xs:duration,           uint32_t, string)
   }                                               \
 }
 
-#define ASSERT2(expression) if(!(expression)) return NULL
+#define ASSERT2(expression, error_code) if(!(expression)) return error_code
+
+#define ASSERT3(expression) if(!(expression)) return NULL
 
 #define ALL_XML_CONTENT_TYPES       \
   ADD_CONTENT(EN_NO_XML_DATA_TYPE, XML element does not hold the content)    \
@@ -87,12 +89,6 @@ typedef enum
 /*
  *  -------------------------------- STRUCTURE --------------------------------
  */
-
-typedef struct
-{
-  char* String;   // This pointer can not be const because this can be created dynamically.
-  size_t Length;
-}string_t;
 
 typedef struct
 {
@@ -169,8 +165,8 @@ typedef struct
 /*
  *  ---------------------------- EXPORTED FUNCTION ----------------------------
  */
-extern const char* extract_content(const xml_content_t* const content,
+extern xml_parse_result_t extract_content(const xml_content_t* const content,
                             void* target, const char* source,
-                            char token);
+                            size_t length);
 
 #endif // XML_CONTENT_H
